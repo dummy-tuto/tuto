728d00fac69ba70e34d31cbdad6c8ce251ea47d5
Brice <brice@shadowrobot.com>
2019-06-25 12:22:28 +0100

    feature 2 implemented and ready to merge

---
+++ substraction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def subtract(first_number, second_number):
+    """
+        Function subtracting one number to another.
+
+        @param first_number: Float or Integer corresponding to the minued
+        @param second_number: Float or Integer corresponding to the subtrahend
+
+        @return: Float or Integer corresponding to the difference
+    """
+    return first_number - second_number
